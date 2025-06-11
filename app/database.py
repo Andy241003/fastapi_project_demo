abf20f99a20db38d2989ee88cdbb91ca0db46607
du241003 <dunguyen241003@gmail.com>
2025-06-11 16:55:04 +0700

    update thu 5

--- app/database.py
+++ app/database.py
@@ -3,9 +3,9 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()  # Đảm bảo nó nạp file .env
 
-MYSQL_URL = os.getenv("MYSQL_URL")  # ví dụ: mysql+pymysql://user:pass@host/dbname
+MYSQL_URL = os.getenv("MYSQL_URL")
 
 engine = create_engine(MYSQL_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
